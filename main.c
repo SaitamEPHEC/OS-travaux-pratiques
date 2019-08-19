3f9f07f718fe538c8efe3adafa6e91c2f4292e2c
Gassmann Mathias <43479118+SaitamEPHEC@users.noreply.github.com>
2019-08-19 23:53:43 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/select.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+#include "lib.h"
+
+int main(){
+
+    system("clear");
+    Menu1();
+
+return 0;
+}
