c2e5d66d363d9bb7c633a45fd87b4b141e86f998
Gassmann Mathias <43479118+SaitamEPHEC@users.noreply.github.com>
2019-08-24 15:09:06 +0200

    Update f1.h

--- f1.h
+++ f1.h
@@ -332,7 +332,7 @@ void tryShowsecteur(Tsecteur tabSect[],int comptSect){
 int EGetNbr(){
 
     srand(time(NULL) ^ (getpid()<<16));
-    int Tvoitures = 12 + (int)rand() / ((int) RAND_MAX / (6));
+    int Tvoitures = 14 + (int)rand() / ((int) RAND_MAX / (6));
     //printf(" NB VOITURE %d  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",Tvoitures );
     return(Tvoitures);
 
